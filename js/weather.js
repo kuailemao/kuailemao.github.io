f8e9a84bcf3fc5ed0e455d2c1fafcc0f2e81ff9b
kuailemao <3490223402@qq.com>
2022-08-25 00:08:17 +0800

    Site updated: 2022-08-25 00:08:16

---
+++ js/weather.js
@@ -0,0 +1,23 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "FFFFFF",
+    "tmpSize": "16",
+    "cityColor": "FFFFFF",
+    "citySize": "16",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "fixed": "true",
+    "vertical": "top",
+    "horizontal": "left",
+    "left": "200",
+    "top": "10",
+    "key": "abea74cfbb584322b7907064555e00e4"
+  }
+}
